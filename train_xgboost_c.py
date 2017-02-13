9a5d082528a75aba15532a8b65b98f52a03dd515
ana silvia <ana.csilva@ugent.be>
2017-02-13 13:11:14 +0100

    read feature vector saved as .h5

--- train_xgboost_c.py
+++ train_xgboost_c.py
@@ -27,18 +27,23 @@ def fair_obj(preds, dtrain):
 
 def main():
 
-	parser = argparse.ArgumentParser(description='XGBoost training')    
-	parser.add_argument('vectors',metavar='<_vectors.pkl>',
-					 help='feature vector file')
+	parser = argparse.ArgumentParser(description='XGBoost training')
+	parser.add_argument('vectors',metavar='<_vectors.pkl or .h5>',
+	         help='feature vector file')
 	parser.add_argument('type',metavar='<type>',
-					 help='model type')
-	parser.add_argument('-c',metavar='INT', action="store", dest='num_cpu', default=23, 
-					 help='number of cpu\'s to use')
+	         help='model type')
+	parser.add_argument('-c',metavar='INT', action="store", dest='num_cpu', default=23,
+	         help='number of cpu\'s to use')
 	args = parser.parse_args()
-	
+
 	sys.stderr.write('loading data\n')
-	
-	vectors = pd.read_pickle(args.vectors)
+
+	if args.vectors.split('.')[-1] == 'pkl':
+	  vectors = pd.read_pickle(args.vectors)
+	elif args.vectors.split('.')[-1] == 'h5':
+	  vectors = pd.read_hdf(args.vectors, 'table')
+	else:
+	  print "unsuported feature vector format"
 	vectors.dropna(inplace=True)
 	print len(vectors)
 	print vectors.head()
@@ -53,13 +58,13 @@ def main():
 	#targets['target'] = np.log2(targets['target']+0.001)
 	#targets['target'] = np.sqrt(targets['target'])
 	#print targets.target.values
-			
+
 	#vectors = vectors[targets.target>0]
 	#psmids = psmids[targets.target>0].PSMid
 	#targets = targets[targets.target>0]
 
 	#psmids = psmids.PSMid
-	
+
 	#selecting charge +2 peptides only!!
 	np.random.seed(1)
 	upeps = psmids.unique()
@@ -68,20 +73,20 @@ def main():
 
 	#creating train/test split
 	#numTrain = int(0.8*len(vectors))
-	
+
 	#train_vectors = vectors.iloc[:numTrain]
 	#train_targets = targets.iloc[:numTrain]
 	#test_vectors = vectors.iloc[numTrain:]
 	#test_targets = targets.iloc[numTrain:]
-	
+
 	test_psms = upeps[:int(num_psms*0.2)]
-	
-	
+
+
 	#with open("testpsm","w") as f:
 	#	for t in test_psms:
 	#		f.write("%s\n"%t)
 	#dddd
-		
+
 	test_vectors = vectors[psmids.isin(test_psms)]
 	test_targets = targetsB[psmids.isin(test_psms)]
 	train_vectors = vectors[~psmids.isin(test_psms)]
@@ -94,24 +99,24 @@ def main():
 
 	train_psmids = train_vectors.pop("psmid")
 	test_psmids = test_vectors.pop("psmid")
-	
+
 	train_vectors = train_vectors.astype(np.float32)
 	test_vectors = test_vectors.astype(np.float32)
 
-	
+
 	#train_targets.hist()
 	#plt.show()
-	
+
 	print len(train_vectors)
 	print len(test_vectors)
-	
+
 	sys.stderr.write('loading data done\n')
-		
+
 	#rename features to understand decision tree dump
 	train_vectors.columns = ['Feature'+str(i) for i in range(len(train_vectors.columns))]
 	test_vectors.columns = ['Feature'+str(i) for i in range(len(train_vectors.columns))]
 	numf = len(train_vectors.columns.values)
-	
+
 	#create XGBoost datastructure
 	sys.stderr.write('creating DMatrix\n')
 	#xtrain = xgb.DMatrix(train_vectors, label=train_targets['y_4'])
@@ -119,10 +124,10 @@ def main():
 	xtrain = xgb.DMatrix(train_vectors, label=train_targets)
 	xeval = xgb.DMatrix(test_vectors, label=test_targets)
 	sys.stderr.write('creating DMatrix done\n')
-	
+
 	evallist  = [(xeval,'eval')]
-	
-	
+
+
 	#set XGBoost parameters; make sure to tune well!
 	param = {"objective":"reg:linear",
 	         "nthread":int(args.num_cpu),
@@ -137,22 +142,22 @@ def main():
 			 #"scale_pos_weight":num_neg/num_pos
 			 #"scale_pos_weight":2
 	         }
-	plst = param.items()          
+	plst = param.items()
 	plst += [('eval_metric', 'rmse')]
-	
+
 	#train XGBoost
 	#bst = xgb.cv( plst, xtrain, 200,nfold=5,callbacks=[xgb.callback.print_evaluation(show_stdv=False),xgb.callback.early_stop(3)])
 	bst = xgb.train( plst, xtrain, 2000, evallist,early_stopping_rounds=10)
 	#bst = xgb.train( plst, xtrain, 30, evallist)
-	
+
 	#save model
 	bst.save_model(args.vectors+'.xgboost')
-	
+
 	#bst = xgb.Booster({'nthread':23}) #init model
 	#bst.load_model(args.vectors+'.xgboost') # load data
 
 
-	
+
 	#get feature importances
 	importance = bst.get_fscore()
 	importance = sorted(importance.items(), key=operator.itemgetter(1))
@@ -161,13 +166,13 @@ def main():
 		for feat,n in importance[:]:
 			ll.append(feat)
 			f.write(feat + "\t" + str(n) + '\n')
-	
+
 	sys.stderr.write("[")
 	for l in ll:
 		sys.stderr.write("'"+l+"',")
-	
+
 	predictions = bst.predict(xeval)
-	
+
 	tmp = pd.DataFrame()
 	tmp['target'] = list(test_targets.values)
 	tmp['predictions'] = predictions
@@ -177,12 +182,12 @@ def main():
 	tmp.to_pickle('predictions.pkl')
 
 	convert_model_to_c(bst,args,numf)
-		
+
 	for ch in range(8,20):
 		print "len %i" % ch
 		n1 = 0
 		n2 = 0
-		tmp3 = tmp[tmp.peplen==ch]		
+		tmp3 = tmp[tmp.peplen==ch]
 		for pid in tmp3.psmid.unique().values:
 			tmp2 = tmp3[tmp3.psmid==pid]
 			print pid
@@ -193,15 +198,15 @@ def main():
 			#print n1
 			n2+=1
 		print float(n1)/n2
-	
+
 	#plt.scatter(x=test_targets,y=predictions)
 	#plt.show()
 
 	#dump model to .c code
-	
+
 def convert_model_to_c(bst,args,numf):
 	#dump model and write .c file
-	bst.dump_model('dump.raw.txt') 
+	bst.dump_model('dump.raw.txt')
 	num_nodes = []
 	mmax = 0
 	with open('dump.raw.txt') as f:
@@ -212,7 +217,7 @@ def convert_model_to_c(bst,args,numf):
 				else:
 					num_nodes.append(mmax+1)
 					mmax = 0
-				continue			
+				continue
 			l=int(row.rstrip().replace(' ','').split(':')[0])
 			if l > mmax:
 				mmax = l
@@ -230,13 +235,13 @@ def convert_model_to_c(bst,args,numf):
 					forest.append(tree)
 					tree = [0]*num_nodes[b]
 					b+=1
-				continue		
-			#if b == len(num_nodes)-10: break	
+				continue
+			#if b == len(num_nodes)-10: break
 			l=row.rstrip().replace(' ','').split(':')
 			if l[1][:4] == "leaf":
 				tmp = l[1].split('=')
 				tree[int(l[0])] = [-1,float(tmp[1]),-1,-1] #!!!!
-			else:	
+			else:
 				tmp = l[1].split('yes=')
 				tmp[0]=tmp[0].replace('[Features','')
 				tmp[0]=tmp[0].replace('[Feature','')
@@ -247,7 +252,7 @@ def convert_model_to_c(bst,args,numf):
 				tmp4 = tmp3[1].split(',')
 				tree[int(l[0])] = [int(tmp2[0]),int(math.ceil(float(tmp2[1]))),int(tmp3[0]),int(tmp4[0])]
 		forest.append(tree)
-	
+
 		tmp = args.vectors.replace('.','_')
 		tmp2 = tmp.split('/')
 		with open(tmp+'_c.c','w') as fout:
@@ -258,14 +263,14 @@ def convert_model_to_c(bst,args,numf):
 			for tt in range(len(forest)):
 				fout.write(tree_to_code(forest[tt],0,1))
 			fout.write("\nreturn s;}\n")
-	
+
 		with open(tmp+'.pyx','w') as fout:
 			fout.write("cdef extern from \"" + tmp2[-1] + "_c.c\":\n")
 			fout.write("\tfloat score_%s(short unsigned short[%i] v)\n\n"%(args.type,numf))
 			fout.write("def myscore(sv):\n")
 			fout.write("\tcdef unsigned short[%i] v = sv\n"%numf)
 			fout.write("\treturn score_%s(v)\n"%args.type)
-	
+
 	#os.remove('dump.raw.txt')
 
 
@@ -275,22 +280,19 @@ def tree_to_code(tree,pos,padding):
 		if tree[pos][1] < 0:
 			return p+"s = s %f;\n"%tree[pos][1]
 		else:
-			return p+"s = s + %f;\n"%tree[pos][1]			
+			return p+"s = s + %f;\n"%tree[pos][1]
 	return p+"if (v[%i]<%i){\n%s}\n%selse{\n%s}"%(tree[pos][0],tree[pos][1],tree_to_code(tree,tree[pos][2],padding+1),p,tree_to_code(tree,tree[pos][3],padding+1))
-	
+
 def print_logo():
-	logo = """                                   
- _____ _____ _____ _____ _____ 
+	logo = """
+ _____ _____ _____ _____ _____
 |_   _| __  |  _  |     |   | |
   | | |    -|     |-   -| | | |
   |_| |__|__|__|__|_____|_|___|
-                               
+
            """
 	print logo
 
 if __name__ == "__main__":
 	print_logo()
-	main()        
-
-	
-	
+	main()
