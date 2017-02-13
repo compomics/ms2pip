fbcce3694a4443d79471af839ccb242575650f0c
asticker <Adriaan.Sticker@UGent.be>
2017-02-13 18:11:42 +0100

    add numpy import in setup.py for compilation
    does not want to compile without numpy import

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("ms2pipfeatures_pyx.pyx")
+    ext_modules = cythonize("ms2pipfeatures_pyx.pyx"),
+    include_dirs=[numpy.get_include()]
 )
