c3a8794c366c3c82b5ea0439051bf8c3faea09fa
ana silvia <ana.csilva@ugent.be>
2017-02-15 13:42:57 +0100

    separated test into feature and targets

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,9 +1,15 @@
 from subprocess import call
 import pandas as pd
 
-def test_get_feature_vectors():
-    call(['python', '../ms2pipC.py', '-s', 'hard_test2.mgf', '-w', 'test', 'test.PEPREC'])
-    target_data = pd.read_pickle('target_features.pkl')
-    test_data = pd.read_pickle('test_vectors.pkl')
+# Run ms2pipC to extract features and targets from an .mgf and .PEPREC files
+call(['python', '../ms2pipC.py', '-s', 'hard_test2.mgf', '-w', 'test', 'test.PEPREC'])
+test_data = pd.read_pickle('test_vectors.pkl')
 
-    assert test_data.equals(target_data)
+# Load target values
+target_data = pd.read_pickle('target_features.pkl')
+
+def test_get_feature_vectors():
+    assert test_data[test_data.columns[:-3]].equals(target_data[target_data.columns[:-3]])
+
+def test_get_targets():
+    assert test_data[test_data.columns[-3:]].equals(target_data[target_data.columns[-3:]])
