930f5febd3cf7e0e5c0ac62270332cf4bbe2610d
ana silvia <ana.csilva@ugent.be>
2017-02-16 11:17:52 +0100

    added target file in .h5; updated target csv and tests

--- tests/test_features.py
+++ tests/test_features.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 # Run ms2pipC to extract features and targets from an .mgf and .PEPREC files
 call(['python', '../ms2pipC.py', '-s', 'hard_test2.mgf', '-w', 'test', 'test.PEPREC'])
-test_data = pd.read_pickle('test_vectors.pkl')
-
+call(['ls'])
+test_data = pd.read_hdf('test_vectors.h5', 'table')
 # Load target values
-target_data = pd.read_pickle('target_features.pkl')
+target_data = pd.read_hdf('target_vectors.h5', 'table')
 
 def test_get_feature_vectors():
     assert test_data[test_data.columns[:-3]].equals(target_data[target_data.columns[:-3]])
 
 def test_get_targets():
     assert test_data[test_data.columns[-3:]].equals(target_data[target_data.columns[-3:]])
+
+call(['rm', 'test_vectors.h5'])
