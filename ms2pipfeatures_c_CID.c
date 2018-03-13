f187a9b3c5d8409f89d0613e50fefbd5b28aa177
Ralf <RalfG@hotmail.be>
2018-03-13 16:17:59 +0100

    Bugfix for wrong mz calculations for N/C-terminally modified peptides

--- ms2pipfeatures_c_CID.c
+++ ms2pipfeatures_c_CID.c
@@ -99,16 +99,25 @@ float* ms2pip_get_mz(int peplen, unsigned short* modpeptide)
 	int i,j;
 	float mz;
 	j=0;
-	mz = modpeptide[0];
+
+	mz = 0;
+	if (modpeptide[0] != 0) {
+		mz = amino_masses[modpeptide[0]];
+	}
 	for (i=1; i < peplen; i++) {
 		mz += amino_masses[modpeptide[i]];
-		membuffer[j++] = mz+1.007236;
+		membuffer[j++] = mz + 1.007236;
 	}
-	mz = modpeptide[-1];
+
+	mz = 0;
+	if (modpeptide[peplen+1] != 0) {
+		mz = amino_masses[modpeptide[peplen+1]];
+	}
 	for (i=peplen; i > 1; i--) {
 		mz += amino_masses[modpeptide[i]];
-		membuffer[j++] = 18.0105647+mz+1.007236;
+		membuffer[j++] = 18.0105647 + mz + 1.007236;
 	}
+
 	return membuffer;
 }
 
