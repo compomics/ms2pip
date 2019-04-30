19e7bd6959b2a5338af14b59f9fc4495a86ad2ff
Sven Degroeve <sven.degroeve@ugent.be>
2019-04-30 16:37:32 +0200

    added script for running kuster synth peptides (run_synth_kuster)

---
+++ run_synth_kuster.py
@@ -0,0 +1,21 @@
+import sys
+import os.path
+import multiprocessing
+
+def runM(dn,x):
+	os.system("python3.5 ms2pipC.py -m 1 -c config.file -w %s.h5 -s %s %s"% (dn,dn,dn.replace("/mgf/","/peprecs/")+".peprecF"))
+
+myPool = multiprocessing.Pool(24)
+
+for dn in sys.argv[1:]:		
+	t = myPool.apply_async(runM, args=(dn,0))
+
+myPool.close()
+myPool.join()
+
+
+
+		
+
+
+
