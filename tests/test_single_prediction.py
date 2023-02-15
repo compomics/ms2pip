ea0936c12bd9ae37f4565c7eeab0f0d5ad547587
RalfG <ralfg@hotmail.be>
2023-02-15 14:02:25 +0100

    Remove obsolete tests for SinglePrediction

--- tests/test_single_prediction.py
+++ tests/test_single_prediction.py
@@ -1,5 +1,6 @@
+from ms2pip.exceptions import (InvalidModificationFormattingError,
+                               InvalidPeptideError)
 from ms2pip.single_prediction import SinglePrediction
-from ms2pip.exceptions import InvalidPeptideError, InvalidModificationFormattingError
 
 MOD_TEST_CASES = {
     "": True,
@@ -30,22 +31,5 @@ PEPTIDE_TEST_CASES = {
 
 
 class TestSinglePrediction:
-    def test_validate_mod_string(self):
-        for mod_string, is_valid in MOD_TEST_CASES.items():
-            try:
-                SinglePrediction._validate_mod_string(mod_string)
-            except InvalidModificationFormattingError:
-                passed = False
-            else:
-                passed = True
-            assert passed == is_valid, f"{mod_string} incorrectly marked as {passed}"
-
-    def test_validate_sequence(self):
-        for peptide, is_valid in PEPTIDE_TEST_CASES.items():
-            try:
-                SinglePrediction._validate_sequence(peptide)
-            except InvalidPeptideError:
-                passed = False
-            else:
-                passed = True
-            assert passed == is_valid, f"{peptide} incorrectly marked as {passed}"
+    # TODO: Implement tests
+    pass
