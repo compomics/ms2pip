94d1939ca707a0afbfd3ceefad0256d272057f40
Ralf Gabriels <ralfg@hotmail.be>
2024-03-05 17:47:45 +0000

    timsrust: Wait to add optional dependency until on PyPI; version bump

--- ms2pip/__init__.py
+++ ms2pip/__init__.py
@@ -1,7 +1,7 @@
 # isort: skip_file
 """MS2PIP: Accurate and versatile peptide fragmentation spectrum prediction."""
 
-__version__ = "4.0.0-dev7"
+__version__ = "4.0.0-dev8"
 
 from warnings import filterwarnings
 
