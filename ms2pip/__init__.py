058d84060337f0a2a11926220265c0c44ddffb84
RalfG <ralfg@hotmail.be>
2024-03-27 17:16:48 +0100

    Version bump

--- ms2pip/__init__.py
+++ ms2pip/__init__.py
@@ -1,7 +1,7 @@
 # isort: skip_file
 """MS2PIP: Accurate and versatile peptide fragmentation spectrum prediction."""
 
-__version__ = "4.0.0-dev8"
+__version__ = "4.0.0-dev9"
 
 from warnings import filterwarnings
 
