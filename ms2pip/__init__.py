dcce1ac603127e9c5dbe784a75217722d839c184
RalfG <ralfg@hotmail.be>
2024-04-08 15:08:49 +0200

    Update dependencies; version bump

--- ms2pip/__init__.py
+++ ms2pip/__init__.py
@@ -1,7 +1,7 @@
 # isort: skip_file
 """MS2PIP: Accurate and versatile peptide fragmentation spectrum prediction."""
 
-__version__ = "4.0.0-dev9"
+__version__ = "4.0.0-dev10"
 
 from warnings import filterwarnings
 
