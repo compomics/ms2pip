97889c90a0f6548083ec6c3bcf76196e72f47965
ArthurDeclercq <arre_declercq@hotmail.com>
2026-01-19 14:24:45 +0100

    version bump

--- ms2pip/__init__.py
+++ ms2pip/__init__.py
@@ -1,7 +1,7 @@
 # isort: skip_file
 """MS2PIP: Accurate and versatile peptide fragmentation spectrum prediction."""
 
-__version__ = "4.1.1"
+__version__ = "4.2.0"
 
 from warnings import filterwarnings
 
@@ -15,7 +15,7 @@ from ms2pip.core import (  # noqa: F401 E402
     predict_batch,
     predict_library,
     correlate,
-    process_observed_spectra,
+    process_MS2_spectra,
     get_training_data,
     annotate_spectra,
     download_models,
